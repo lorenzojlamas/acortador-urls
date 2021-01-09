b614fd750f389f1f68d6d0ec6f02eba1cbc37c3d
Lorenzo Jose Lamas <lorenzolamas@MBPdeLorenzo2.lan>
2021-01-09 16:53:37 -0300

    feat: inicial approuch for clipUrl

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,63 @@
-const functions = require('firebase-functions');
+"use strict";
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const nanoId = require("nanoid");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// TODO: podría ser configurable por variables de entorno.
+const lengthOfShortUrls = 6;
+
+exports.clipUrl = functions.https.onRequest((req, res) => {
+
+    const url = req.query.url;
+
+    // TODO: agregar trace al log
+    // TODO: Definir librería y forma de logging
+    console.info("Request clip url: ", url);
+
+    const database = admin.database().ref('urls');
+
+
+    // TODO: usar async functions, Callback hell !!!!
+    createShortUrlInDatabese(database, url).then(clipedUrl => {
+        res.status(200).send(clipedUrl);
+    }).catch(error => {
+        // TODO: manejo de errores
+        res.status(500).send(error.message);
+    });
+
+});
+
+function createShortUrlInDatabese(database, url) {
+    return new Promise((resolve, reject) => {
+        var clipedUrl;
+        clipedUrl = nanoId.nanoid(lengthOfShortUrls);
+        console.debug("ClipedUrl: ", clipedUrl);
+
+        try {
+            database.get(clipedUrl).then((value) => {
+                console.log(value);
+                if (value == null) {
+                    pushAndResolve(database, url, clipedUrl, resolve);
+                } else {
+                    console.info('Value allready exist: ', value);
+                    createShortUrlInDatabese(database, url).then((newClipedUrl) => {
+                        resolve(newClipedUrl);
+                    });
+                }
+            });
+        } catch (error) {
+            reject(error);
+        }
+
+    });
+}
+
+function pushAndResolve(database, url, clipedUrl, resolve) {
+    database.push({
+        clipedUrl: url
+    }).then((value) => {
+        console.debug('Value saved', value);
+        resolve(clipedUrl);
+    });
+}
---
+++ functions/test/test.spec.js
@@ -0,0 +1,87 @@
+const admin = require("firebase-admin");
+const nanoId = require("nanoid");
+admin.initializeApp = jest.fn();
+const test = require("firebase-functions-test")();
+
+// TODO: renombrar
+const cliper = require("../index");
+
+
+describe("clipUrl", () => {
+    let oldDatabase;
+
+    beforeEach(() => {
+        oldDatabase = admin.database;
+    });
+
+    afterEach(() => {
+        admin.database = oldDatabase;
+    });
+
+    it("Given a request with url in the query params when the method is called then a short identifier is returned", done => {
+
+        const databaseStub = jest.fn(() => {
+            return {
+                ref: jest.fn(location => {
+                    return {
+                        push: jest.fn(val => Promise.resolve({ ref: location })),
+                        get: jest.fn(val => new Promise((resolve, reject) => {
+                            if (val === '123456') {
+                                resolve({ 123456: 'someUrl' });
+                            } else {
+                                resolve(null);
+
+                            }
+                        }))
+
+                    };
+                })
+            };
+        });
+        Object.defineProperty(admin, "database", { get: () => databaseStub });
+
+        const url = "http://someUrlTo.cut";
+        const mockRequest = {
+            query: {
+                url: url
+            }
+        };
+        const mockResponse = {
+            status: (code) => {
+                expect(code).toEqual(200);
+                return {
+                    send: (response) => {
+                        expect(response.length).toBe(6);
+                        done();
+                    }
+                }
+            }
+        };
+        cliper.clipUrl(mockRequest, mockResponse);
+    });
+
+    it("Given a request with a url in queryparams and the first short url obtained is already saved when the method is called, then a short identifier is returned", done => {
+
+        const sanoIdStub = jest.fn().mockReturnValueOnce('123456').mockReturnValueOnce('654321');
+        Object.defineProperty(nanoId, "nanoid", { get: () => sanoIdStub });
+
+        const url = "http://someUrlTo.cut";
+        const mockRequest = {
+            query: {
+                url: url
+            }
+        };
+        const mockResponse = {
+            status: (code) => {
+                expect(code).toEqual(200);
+                return {
+                    send: (response) => {
+                        expect(response.length).toBe(6);
+                        done();
+                    }
+                }
+            }
+        };
+        cliper.clipUrl(mockRequest, mockResponse);
+    })
+});
